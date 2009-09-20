7d1b192a6324d754683d722130a7d300a074f695
Jeremy Zawodny <Jeremy@Zawodny.com>
2009-09-19 21:39:19 -0700

    added module

---
+++ lib/Text/SpotSig.pm
@@ -0,0 +1,98 @@
+package Text::SpotSig;
+
+use strict;
+use Exporter;
+
+our $VERSION = '0.1';
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw();
+our @EXPORT_OK = qw();
+our %EXPORT_TAGS = ();
+
+1;
+
+my @defaultStopwords = qw(the an and or as of am your we a if is to our his her in);
+
+sub new {
+	my $class = shift;
+
+	my $self = {
+		foo       => 1,
+		debug     => 0,
+		regex     => undef,
+		stopwords => [@defaultStopwords],
+	};
+
+
+	return bless $self, $class;
+};
+
+## set stopwords to the list we're passed
+
+sub setStopwords {
+	my $self = shift;
+	my $ref  = shift;
+
+	## TODO: make sure we're passed an array and stuff
+
+	$self->{stopwords} = $ref;
+	$self->{regex} = undef;
+}
+
+## perform analysis on text that we're passed
+
+sub analyze {
+	my $self = shift;
+	my $text = shift;
+	my @sigs = ();
+
+	## clean the text up, removing everything except word characters
+	## and basic punctuation.
+
+	my %stopword = map { $_ => 1 } @{$self->{stopwords}};
+	my $sig;
+	my $cnt = 0;
+
+	## find the sigs
+	while ($text =~ m/(\w+)/g) {
+		my $word = lc $1;
+		print STDERR "word: $word\n" if $self->{debug};
+
+		if ($stopword{$word}) {
+			if (defined $sig) {
+				if ($cnt > 1) {
+					push @sigs, $sig;
+					print STDERR "sig : $sig\n" if $self->{debug};
+				}
+				$sig = undef;
+				$cnt = 0;
+			}
+			$sig = $word;
+			$cnt++;
+		}
+		else {
+			if (defined $sig) {
+				$sig .= " $word";
+				$cnt++;
+			}
+		}
+	}
+
+	## any left?
+	if (defined $sig) {
+		if ($cnt > 1) {
+			push @sigs, $sig;
+			print STDERR "sig : $sig\n" if $self->{debug};
+		}
+	}
+
+	return \@sigs;
+}
+
+sub version {
+	my $self = shift;
+	return $VERSION;
+}
+
+__END__
